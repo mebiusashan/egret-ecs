98b4cd117613a47c91451d3d6507cc1d9c8edee4
mebius <mebius@ashan.org>
2018-11-14 21:15:43 +0800

    添加销毁组件

---
+++ src/src/ecs/components/DestroyComponent.ts
@@ -0,0 +1,14 @@
+//////////////////////////
+//
+// ashan ashan@mebius.org
+//
+//////////////////////////
+
+
+/**
+ * 销毁
+ */
+class DestroyComponent implements entitas.IComponent, IRestore {
+    public restore(): void {
+    }
+}
