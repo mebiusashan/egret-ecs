52b9ebffc856987d100635ef30b22d9b0bfc7b08
mebius <mebius@ashan.org>
2018-11-14 21:15:52 +0800

    添加gameobject

---
+++ src/src/ecs/core/GameEntity.ts
@@ -0,0 +1,71 @@
+//////////////////////////
+//
+// ashan ashan@mebius.org
+//
+//////////////////////////
+
+
+interface IRestore {
+    restore: () => void;
+}
+
+interface IView {
+    removeView: () => void;
+}
+
+class GameEntity extends entitas.Entity {
+
+    public readonly ecscontext: EntitasContext | null = null;
+
+    protected constructor(componentsEnum, totalComponents, ecsctx: EntitasContext) {
+        super(componentsEnum, totalComponents);
+        this.ecscontext = ecsctx;
+    }
+
+    public active(): GameEntity {
+        this.onComponentRemoved.add(this.$onComponentRemoved);
+        return this;
+    }
+
+    private $onComponentRemoved = (entity: entitas.Entity, index: number, previousComponent: entitas.IComponent): void => {
+        const cast: entitas.IComponent | any = previousComponent;
+        if (cast['restore']) {
+            cast['restore']();
+        }
+        if (cast['removeView']) {
+            cast['removeView']();
+        }
+        this.ecscontext.restoreAs(previousComponent);
+    }
+
+    public addAs<T>(klass: new () => T): T | null {
+        if (!this.ecscontext) {
+            return null;
+        }
+        const cid: number = this.ecscontext.cid(klass);
+        const resAsCom = this.ecscontext.createAs(klass);
+        this.addComponent(cid, resAsCom as entitas.IComponent);
+        return resAsCom;
+    }
+
+    public getAs<T>(klass: new () => T): T {
+        const cid: number = this.ecscontext.cid(klass);
+        return this.getComponent(cid) as T;
+    }
+
+    public removeAs<T>(klass: new () => T): GameEntity {
+        const cid: number = this.ecscontext.cid(klass);
+        if (this.hasComponent(cid)) {
+            this.removeComponent(cid);
+        }
+        return this;
+    }
+
+    public hasAs<T>(klass: new () => T): T | null {
+        const cid: number = this.ecscontext.cid(klass);
+        if (this.hasComponent(cid)) {
+            return this.getComponent(cid) as T;
+        }
+        return null;
+    }
+}
---
+++ src/src/ecs/core/GameObject.ts
@@ -0,0 +1,40 @@
+//////////////////////////
+//
+// ashan ashan@mebius.org
+//
+//////////////////////////
+
+class GameObject extends GameEntity {
+
+    static readonly ZeroPosition = new egret.Point(0, 0);
+
+    public static create(ecsctx: EntitasContext, name: string): GameObject {
+        if (!ecsctx) {
+            return null;
+        }
+        const pool = ecsctx.pool;
+        if (pool._reusableEntities.size() === 0) {
+            const entity: GameObject = new GameObject(pool._componentsEnum, pool._totalComponents, ecsctx);
+            pool._reusableEntities.add(entity);
+        }
+        return (pool.createEntity(name).active()) as GameObject;
+    }
+
+    public static destroy(gameObject: GameObject): void {
+        if (gameObject && gameObject._isEnabled) {
+            gameObject.ecscontext.pool.destroyEntity(gameObject);
+        }
+    }
+
+    private constructor(componentsEnum, totalComponents, ecsctx: EntitasContext) {
+        super(componentsEnum, totalComponents, ecsctx);
+    }
+
+    public setDestroy(): GameObject {
+        if (!this.hasAs(DestroyComponent)) {
+            this.addAs(DestroyComponent);
+        }
+        return this;
+    }
+
+}
