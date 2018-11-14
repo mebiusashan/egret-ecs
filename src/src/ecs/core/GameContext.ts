b02cb0d02b35bf52c6e5dad88c141781aea7245a
mebius <mebius@ashan.org>
2018-11-14 21:05:56 +0800

    添加游戏核心上下文

---
+++ src/src/ecs/core/GameContext.ts
@@ -0,0 +1,15 @@
+//////////////////////////
+//
+// ashan ashan@mebius.org
+//
+//////////////////////////
+
+
+/**
+ * 游戏核心上下文
+ */
+class GameContext {
+    public dtMs: number = 0;
+    public dtSec: number = 0;
+
+}
